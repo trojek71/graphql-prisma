79e9d143529dad59ba1300cec08e75d1e0ea0e62
Tomek Rojek <tomekr@gmail.com>
2021-03-02 09:53:57 +0100

    part 65  DONE

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,36 +1,52 @@
 const Subscription = {
-    count: {
-        subscribe(parent, args, { pubsub }, info) {
-            let count = 0
+    // count: {
+    //     subscribe(parent, args, { pubsub }, info) {
+    //         let count = 0
 
-            setInterval(() => {
-                count++
-                pubsub.publish('count',{
-                    count
-                })
-            }, 1000)
+    //         setInterval(() => {
+    //             count++
+    //             pubsub.publish('count',{
+    //                 count
+    //             })
+    //         }, 1000)
 
-            return pubsub.asyncIterator('count')
+    //         return pubsub.asyncIterator('count')
 
-        }
-    },
+    //     }
+    // },
     comment: {
-        subscribe(parent, { postId }, { db, pubsub }, info){
-            const post = db.posts.find((post) => post.id === postId && post.published)
+        subscribe(parent, { postId }, { prisma }, info){
+            return prisma.subscription.comment({
+                where:{
+                    node:{
+                        post:{
+                            id:postId
+                        }
+                    }
+                }
+            },info)
+        //     const post = db.posts.find((post) => post.id === postId && post.published)
         
-            if (!post) {
-                throw new Error('Post not found')
-            }
+        //     if (!post) {
+        //         throw new Error('Post not found')
+        //     }
 
-                return pubsub.asyncIterator(`comment ${postId}`)
+        //         return pubsub.asyncIterator(`comment ${postId}`)
 
+        // }
         }
     },
     post:{
-        subscribe(parent, args, { pubsub }, info){
-            return pubsub.asyncIterator(`post`)
+        subscribe(parent, args, { prisma }, info){
+            //return pubsub.asyncIterator(`post`)
+            return prisma.subscription.post({
+                where:{
+                    node:{
+                        published: true
+                    }
+                }
+            },info)
         }
-    }
 }
-
+}
 export { Subscription as default }
